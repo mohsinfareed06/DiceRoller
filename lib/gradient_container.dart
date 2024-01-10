3dfd4705c4e8d4587dbfbffa94e357ea4e5799c0
mohsinfareed06 <Mohsinfareed06@gmail.com>
2024-01-10 16:21:48 +0500

    second

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -8,10 +8,13 @@ class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
 
   final Color color1;
+
   final Color color2;
 
   @override
   Widget build(context) {
+    final Color asd;
+
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
